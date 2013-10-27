af6446bbc33b9431a0116e338c19c5ee49c64428
Ben Roth <spongebucket@gmail.com>
2013-10-26 17:51:14 -0700

    add example app that links against lantern lib

---
+++ iOS/example/lantern-example/lantern-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lantern-example
+//
+//  Created by Ben Roth on 10/26/13.
+//  Copyright (c) 2013 Ben Roth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LAViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) LAViewController *viewController;
+
+@end
---
+++ iOS/example/lantern-example/lantern-example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lantern-example
+//
+//  Created by Ben Roth on 10/26/13.
+//  Copyright (c) 2013 Ben Roth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
