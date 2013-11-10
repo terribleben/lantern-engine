79b8bba700f686f1fd17cabfd358a3b0702002cd
Ben Roth <spongebucket@gmail.com>
2013-11-10 14:40:02 -0800

    simple delay audio processor

---
+++ cpp/audio/processor/ProcessorDelay.cpp
@@ -0,0 +1,87 @@
+//
+//  ProcessorDelay.cpp
+//  lantern
+//
+
+#include <cstdlib>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+#include "ProcessorDelay.h"
+
+ProcessorDelay::ProcessorDelay() : Processor() {
+    length = 0;
+    bufferLength = 0;
+    feedback = 0;
+    buffer = NULL;
+    bufferPointer = NULL;
+}
+
+ProcessorDelay::ProcessorDelay(unsigned int length, float feedback) {
+    this->length = 0;
+    bufferLength = 0;
+    this->feedback = 0;
+    buffer = NULL;
+    bufferPointer = NULL;
+    
+    setLength(length);
+    this->feedback = feedback;
+}
+
+ProcessorDelay::~ProcessorDelay() {
+    if (buffer){
+        free(buffer);
+        buffer = NULL;
+    }
+}
+
+void ProcessorDelay::recompute() {
+    
+}
+
+void ProcessorDelay::setLength(unsigned int length) {
+    unsigned int lengthFrames = length * LANTERN_AUDIO_NUM_CHANNELS;
+    
+    // compute the necessary buffer size to accomodate this length
+    unsigned int actualBufferAllocLength = (LANTERN_AUDIO_SAMPLE_RATE * LANTERN_AUDIO_NUM_CHANNELS) / 10;
+    while (actualBufferAllocLength < lengthFrames) {
+        actualBufferAllocLength *= 2;
+    }
+    
+    if (actualBufferAllocLength > bufferLength) {
+        // existing buffer is insufficient, realloc
+        Sample* newBuffer = (Sample*) malloc(sizeof(Sample) * actualBufferAllocLength);
+        if (buffer) {
+            memcpy(newBuffer, buffer, sizeof(Sample) * std::min(bufferLength, actualBufferAllocLength));
+            bufferPointer = newBuffer + (bufferPointer - buffer);
+            if (bufferPointer > newBuffer + (length * LANTERN_AUDIO_NUM_CHANNELS))
+                bufferPointer = newBuffer;
+            
+            free(buffer);
+            buffer = NULL;
+        } else {
+            memset(newBuffer, 0, sizeof(Sample) * actualBufferAllocLength);
+            bufferPointer = newBuffer;
+        }
+        
+        buffer = newBuffer;
+        bufferLength = actualBufferAllocLength;
+    }
+    
+    this->length = length;
+}
+
+unsigned int ProcessorDelay::getLength() {
+    return length;
+}
+
+Sample ProcessorDelay::process(Sample input) {
+    Sample output = input + (*bufferPointer * feedback);
+    
+    *bufferPointer = output;
+    bufferPointer++;
+    if (bufferPointer > buffer + (length * LANTERN_AUDIO_NUM_CHANNELS))
+        bufferPointer = buffer;
+    
+    return output * gain;
+}
---
+++ cpp/audio/processor/ProcessorDelay.h
@@ -0,0 +1,41 @@
+//
+//  ProcessorDelay.h
+//
+//  A delay line.
+//  Be aware that this uses RAM proportional to the length of the line
+//  (e.g. a 1-second delay @ 2 channels 44100 sample rate = ~350kb).
+//  Internally it may also alloc more memory than you ask for in order to avoid lots of reallocs.
+//
+//  lantern
+//
+
+#ifndef __LANTERN_PROCESSOR_DELAY_H__
+#define __LANTERN_PROCESSOR_DELAY_H__
+
+#include "Processor.h"
+
+class ProcessorDelay : public Processor {
+public:
+    ProcessorDelay(unsigned int length, float feedback);
+    ~ProcessorDelay();
+    
+    Sample process(Sample input);
+    
+    // length, in frames, of the delay line
+    unsigned int getLength();
+    void setLength(unsigned int);
+    
+    // 0 no feedback, 1 complete feedback
+    float feedback;
+    
+protected:
+    ProcessorDelay();
+    void recompute();
+    
+    unsigned int length;
+    unsigned int bufferLength;
+    Sample* buffer;
+    Sample* bufferPointer;
+};
+
+#endif
