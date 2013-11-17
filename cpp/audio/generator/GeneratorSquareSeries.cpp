e85d6260c45d0162b885f2907334fe76927bb02b
Ben Roth <spongebucket@gmail.com>
2013-11-17 10:58:04 -0800

    make the square series generator far more efficient and useful

--- cpp/audio/generator/GeneratorSquareSeries.cpp
+++ cpp/audio/generator/GeneratorSquareSeries.cpp
@@ -6,63 +6,52 @@
 #include <cstdlib>
 #include "GeneratorSquareSeries.h"
 #include "util_math.h"
+#include <cstdio>
 
 GeneratorSquareSeries::GeneratorSquareSeries() : Generator() {
-    innerCoefficients = NULL;
-    outerCoefficients = NULL;
-    step = 0;
+    harmonics = NULL;
+    constant = (4.0f / M_PI);
     setNumHarmonics(1);
 }
 
 GeneratorSquareSeries::~GeneratorSquareSeries() {
-    if (innerCoefficients) {
-        free(innerCoefficients);
-        innerCoefficients = NULL;
-    }
-    if (outerCoefficients) {
-        free(outerCoefficients);
-        outerCoefficients = NULL;
+    if (harmonics) {
+        delete [] harmonics;
+        harmonics = NULL;
     }
 }
 
 void GeneratorSquareSeries::setNumHarmonics(unsigned int numHarmonics) {
     this->numHarmonics = numHarmonics;
+    
+    if (harmonics) {
+        delete [] harmonics;
+        harmonics = NULL;
+    }
+    harmonics = new GeneratorSinSHM[numHarmonics];
+    
     recompute();
 }
 
 void GeneratorSquareSeries::recompute() {
-    if (innerCoefficients) {
-        free(innerCoefficients);
-        innerCoefficients = NULL;
-    }
-    if (outerCoefficients) {
-        free(outerCoefficients);
-        outerCoefficients = NULL;
-    }
-    
-    innerCoefficients = (float*) malloc(sizeof(float) * numHarmonics);
-    outerCoefficients = (float*) malloc(sizeof(float) * numHarmonics);
-    
     // kth term of the sum at time t:
     // sin(2pi * (2k - 1) * ft) * (1 / (2k - 1)) * (4 / pi).
-    //
-    // inner coefficients are the stuff inside sin(); outer coefficients are all the other crap.
-    
-    float angularFrequency = M_2PI * (freq / LANTERN_AUDIO_SAMPLE_RATE);
+    // That last term is taken care of by a constant inside next().
     
     for (unsigned int kInt = 0; kInt < numHarmonics; kInt++) {
         float k = kInt + 1.0f;
         float sumCoefficient = (2.0f * k) - 1.0f;
-        outerCoefficients[kInt] = 4.0f / (sumCoefficient * M_PI);
-        innerCoefficients[kInt] = angularFrequency * sumCoefficient;
+        harmonics[kInt].setFreq(freq * sumCoefficient);
+        harmonics[kInt].setGain(1.0f / sumCoefficient);
     }
 }
 
 Sample GeneratorSquareSeries::next() {
     Sample sum = 0;
+    
     for (unsigned int k = 0; k < numHarmonics; k++) {
-        sum += outerCoefficients[k] * sin(innerCoefficients[k] * step);
+        sum += harmonics[k].next();
     }
-    step++;
-    return sum;
+    
+    return sum * constant * gain;
 }
--- cpp/audio/generator/GeneratorSquareSeries.h
+++ cpp/audio/generator/GeneratorSquareSeries.h
@@ -1,9 +1,8 @@
 //
 //  GeneratorSquareSeries.h
 //
-//  Generates square waves that look more like an analogue circuit.
+//  Generates square waves using a series of sin harmonics (so more like an actual circuit).
 //  It gets closer to a perfect square wave, and also more expensive to compute, as you increase the number of harmonics.
-//  In particular, there is a call to std::sin() for every harmonic for every sample.
 //
 //  lantern
 //
@@ -12,6 +11,7 @@
 #define __LANTERN_GENERATOR_SQUARE_SERIES_H__
 
 #include "Generator.h"
+#include "GeneratorSinSHM.h"
 
 class GeneratorSquareSeries : public Generator {
 public:
@@ -25,10 +25,9 @@ public:
 private:
     void recompute();
     
-    unsigned int step;
     unsigned int numHarmonics;
-    float* innerCoefficients;
-    float* outerCoefficients;
+    GeneratorSinSHM* harmonics;
+    float constant;
 };
 
 #endif
